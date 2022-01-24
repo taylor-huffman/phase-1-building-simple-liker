7fcae63874822af5705f92c3a33f782b6d893963
Taylor Huffman <taylor@crucialartists.com>
2022-01-24 14:08:24 -0600

    Completed assignment

--- main.js
+++ main.js
@@ -1,9 +1,41 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+const modal = document.getElementById('modal')
+const modalMessage = document.getElementById('modal-message')
 
 // Your JavaScript code goes here!
+let handleServerResponse = (e) => {
+  mimicServerCall()
+  .then(response => {
+    console.log(response)
+    if(response === 'Pretend remote server notified of action!' && e.target.textContent === EMPTY_HEART) {
+      e.target.textContent = FULL_HEART
+      e.target.classList.add('activated-heart')
+    } else {
+      e.target.textContent = EMPTY_HEART
+      e.target.classList.remove('activated-heart')
+    }
+  })
+  .catch(error => {
+    console.log(error)
+    modalMessage.textContent = error
+    modal.classList.remove('hidden')
+    setTimeout(() => {
+      modal.classList.add('hidden')
+    }, 3000)
+  })
+}
+      
+      
 
+
+document.addEventListener('DOMContentLoaded', () => {
+  let likeButton = document.querySelectorAll('span.like-glyph')
+  for (let item of likeButton) {
+    item.addEventListener('click', handleServerResponse)
+  } 
+})
 
 
 
